edb87507f236c377e3f6927d81c1f81847f0c966
Itamar Davidyan <91004029+itamar-owlytics@users.noreply.github.com>
2021-12-29 11:23:14 +0200

    Use class syntax and extends keyword instead of util.inherits (#16)
    `util.inherits` is now deprecated due to issues with how it sets
    the prototype chain, see:  https://github.com/nodejs/node/issues/4179

--- hypothesis/two-data-set.js
+++ hypothesis/two-data-set.js
@@ -2,21 +2,24 @@
 
 const Distribution = require('distributions').Studentt;
 
-const util = require('util');
 const AbstactStudentT = require('./abstact.js');
 
-function StudentT(left, right, options) {
-  AbstactStudentT.call(this, options);
+class StudentT extends AbstactStudentT {
+  
+  constructor(left, right, options) {
+    super(options);
 
-  this._df = left.size + right.size - 2;
-  this._dist = new Distribution(this._df);
+    this._df = left.size + right.size - 2;
+    this._dist = new Distribution(this._df);
 
-  const commonVariance = ((left.size - 1) * left.variance +
-                          (right.size - 1) * right.variance
-                         ) / this._df;
+    const commonVariance = ((left.size - 1) * left.variance +
+                            (right.size - 1) * right.variance
+                          ) / this._df;
 
-  this._se = Math.sqrt(commonVariance * (1 / left.size + 1 / right.size));
-  this._mean = left.mean - right.mean;
+    this._se = Math.sqrt(commonVariance * (1 / left.size + 1 / right.size));
+    this._mean = left.mean - right.mean;
+  }
+
 }
-util.inherits(StudentT, AbstactStudentT);
+
 module.exports = StudentT;
--- hypothesis/welch.js
+++ hypothesis/welch.js
@@ -2,24 +2,26 @@
 
 const Distribution = require('distributions').Studentt;
 
-const util = require('util');
 const AbstactStudentT = require('./abstact.js');
 
-function StudentT(left, right, options) {
-  AbstactStudentT.call(this, options);
+class StudentT extends AbstactStudentT {
+  
+  constructor(left, right, options) {
+    super(options);
 
-  const leftSE = left.variance / left.size;
-  const rightSE = right.variance / right.size;
-  const commonVariance = leftSE + rightSE;
+    const leftSE = left.variance / left.size;
+    const rightSE = right.variance / right.size;
+    const commonVariance = leftSE + rightSE;
 
-  this._df = Math.pow(commonVariance, 2) / (
-    Math.pow(leftSE, 2) / (left.size - 1) +
-    Math.pow(rightSE, 2) / (right.size - 1)
-  );
-  this._dist = new Distribution(this._df);
+    this._df = Math.pow(commonVariance, 2) / (
+      Math.pow(leftSE, 2) / (left.size - 1) +
+      Math.pow(rightSE, 2) / (right.size - 1)
+    );
+    this._dist = new Distribution(this._df);
 
-  this._se = Math.sqrt(commonVariance);
-  this._mean = left.mean - right.mean;
+    this._se = Math.sqrt(commonVariance);
+    this._mean = left.mean - right.mean;
+  }
 }
-util.inherits(StudentT, AbstactStudentT);
+
 module.exports = StudentT;
